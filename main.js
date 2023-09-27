84fdc4091f1f0e6fe74976a2cbfab29753135690
ADL12388 <albertldfx1314@gmail.com>
2023-09-27 08:45:49 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,96 @@
+// change navbar style on scroll
+
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    // change icon
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "fa-solid fa-plus") {
+      icon.className = "fa-solid fa-minus";
+    } else {
+      icon.className = "fa-solid fa-plus";
+    }
+  });
+});
+
+// show/hide nav menu
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+// close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // when windows width  is >= 600px
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+ScrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+ScrollReveal().reveal(
+  ".header__left, .categories__left, .footer__1, .footer__2",
+  {
+    delay: 500,
+    origin: "left",
+  }
+);
+
+ScrollReveal().reveal(
+  ".header__right, .footer__3, .footer__4, .footer__socials, .about__achievements-right",
+  { delay: 500, origin: "right" }
+);
+ScrollReveal().reveal(".categories__right, .courses h2", { delay: 500 });
+ScrollReveal().reveal(
+  ".courses__container, .faqs h2, .testimonials__container h2, .mySwiper, .testimonials .super-slide",
+  {
+    delay: 600,
+  }
+);
+ScrollReveal().reveal(".faq, .footer__copyright", { delay: 600, origin: "up" });
+ScrollReveal().reveal(".courses1 h2 ", {
+  delay: 600,
+  origin: "top",
+});
+ScrollReveal().reveal(".faq, .footer__copyright", { delay: 600, origin: "up" });
+ScrollReveal().reveal(".courses1__container ", {
+  delay: 500,
+  origin: "top",
+});
